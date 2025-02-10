a42cb8ca57461fb32d266ec919157b88a720d2af
Khan Abid <111801271+Khan-Abid786@users.noreply.github.com>
2025-02-10 14:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ uploaded_file = st.file_uploader("Upload an image of a potato leaf...", type=["j
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
 
     # Preprocess image
     processed_image = preprocess_image(image)
